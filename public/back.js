09b021692b07e87f6080744bc46edef09ea19ed8
agus089924 <divaagustinpurba@gmail.com>
2023-03-10 00:21:53 +0700

    commit kedua

--- public/back.js
+++ public/back.js
@@ -1,20 +1,20 @@
 function startlog(){
-    let net1 = document.getElementById('email').value + document.getElementById('password').value;
-    let net2 = ('beli1@gmail.com') + ('beli1123');
-    let net3 = ('beli2@gmail.com') + ('beli2123');
-    let net4 = ('jual1@gmail.com') + ('jual1123');
-    let net5 = ('jual2@gmail.com') + ('jual2123');
+    let log1 = document.getElementById('email').value + document.getElementById('password').value;
+    let log2 = ('beli1@gmail.com') + ('beli1123');
+    let log3 = ('beli2@gmail.com') + ('beli2123');
+    let log4 = ('jual1@gmail.com') + ('jual1123');
+    let log5 = ('jual2@gmail.com') + ('jual2123');
 
-    if (net1 == net2) {
+    if (log1 == log2) {
     let logakses = document.getElementById('akses').innerHTML = 'login sukses';
     }
-    else if (net1 == net3) {
+    else if (log1 == log3) {
         let logakses = document.getElementById('akses').innerHTML = 'login sukses';
     }
-    else if (net1 == net4) {
+    else if (log1 == log4) {
     let logakses = document.getElementById('akses').innerHTML = 'login sukses';
     }
-    else if (net1 == net5) {
+    else if (log1 == log5) {
     let logakses = document.getElementById('akses').innerHTML = 'login sukses';
         }
     else {
